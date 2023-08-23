5c8543347b6bc68cf9b3e61ebd3b537ba99a51d4
John <johnfe@outlook.es>
2023-08-23 17:49:35 -0500

    Avance 1: Creacion proyecto con dependencias web necesarias

---
+++ src/main/java/com/example/RepasoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoFinalApplication.class, args);
+	}
+
+}
